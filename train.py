875163bf45799dcadc45b685e9c05f94b7cfcc84
Mickey Chao <mjchao2@gmail.com>
2016-08-24 22:31:24 -0400

    Add interactivity with IPython

--- train.py
+++ train.py
@@ -3,6 +3,7 @@ Created on Aug 23, 2016
 
 @author: mjchao
 '''
+import IPython
 import model
 import train_config
 
@@ -11,6 +12,14 @@ def main():
     config = train_config.DefaultConfig()
     char_pred_model = model.CharacterPredictorModel(config)
     char_pred_model.Train()
+    _start_shell(locals())
+
+def _start_shell(local_ns=None):
+    user_ns = {}
+    if local_ns:
+        user_ns.update(local_ns)
+    user_ns.update(globals())
+    IPython.start_ipython(argv=[], user_ns=user_ns)
 
 
 if __name__ == "__main__": main()
